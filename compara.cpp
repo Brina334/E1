695cecb74de01f429341f01d45fee9f7d5e1cddf
sabrigodoy <sabrina.godoy.cotera@utelvt.edu.ec>
2022-09-29 17:33:56 -0500

    examen

---
+++ compara.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+//Sabrina Fabiola Godoy Cotera
+using namespace std;
+int main()
+{
+  float f,g;
+  cout<<"Inserte el primer numero:     "; cin>>f;
+  cout<<"Inserte el segundo numero:    "; cin>>g;
+  if(f>g)
+  {
+     cout<<"el primer es  mayor que"<<endl;
+
+  }
+  else   if(f==g)
+  {
+       cout<<f<<" son iguales  "<<g<<"\n";
+     
+  }
+  else
+     {
+       cout<<f<<" es menor que "<<g<<"\n";
+     }
+  
+return (0);
+
+
+
+
+}
